6bed3e4186caaa4dedd553d3fb587e29495658fd
itzcookiie <mayowaadeniyi@hotmail.com>
2021-08-10 20:41:01 +0100

    Fix server by making sure server can find settings config file

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: daphne myproject.asgi:application
+web: daphne app.api.asgi:application
--- app/api/asgi.py
+++ app/api/asgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+import sys
 
 from django.core.asgi import get_asgi_application
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
+os.environ["DJANGO_SETTINGS_MODULE"] = "api.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
 
 application = get_asgi_application()
