5afa17ee151c0333c4b9d2186b70ed2f5c49729a
itzcookiie <mayowaadeniyi@hotmail.com>
2021-08-10 22:12:11 +0100

    Make settings file production ready

--- app/api/settings.py
+++ app/api/settings.py
@@ -21,12 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-8#jqc9ogju*g8w*c+^awng=t&s^5i_=2$r1a_%f2f$^j4-5qvd"
-)
+SECRET_KEY = os.environ.get("SECRET_KEY", "SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", True)
 
 ALLOWED_HOSTS = ["serene-tundra-70827.herokuapp.com"]
 
