ad549460239b3ac098a7752b4c0404dcede5ebe2
itzcookiie <mayowaadeniyi@hotmail.com>
2021-08-13 15:46:40 +0100

    Fix home page routing

--- app/api/urls.py
+++ app/api/urls.py
@@ -5,8 +5,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("anime/", include("anime.urls")),
     path("admin/", admin.site.urls),
-    path(
-        "<path:resource>",
+    re_path(
+        r"^",
         TemplateView.as_view(template_name="anime/index.html"),
     ),
 ]
