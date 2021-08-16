f2da8213e967ec7315f20aa758b420cdac35f802
itzcookiie <mayowaadeniyi@hotmail.com>
2021-08-16 23:13:05 +0100

    start api refactor using class

--- app/anime/views.py
+++ app/anime/views.py
@@ -1,3 +1,4 @@
+import logging
 from random import random
 
 import requests
@@ -5,10 +6,42 @@ from django.core.cache import cache
 from django.http import HttpResponse, JsonResponse
 from ratelimit.decorators import ratelimit
 
+logger = logging.getLogger("mylogger")
+
+
+class APIService:
+    def __init__(self, url, params, error_message):
+        self.url = url
+        self.params = params
+        self.cache = {}
+        self.headers = {}
+        self.response = None
+        self.error_message = {"quote": error_message, "status": "Error"}
+
+    def get(self):
+        self.response = requests.get(
+            self.url, headers=self.headers, params=self.params
+        )
+
+    def valid_response(self):
+        return (
+            self.response.status_code == requests.codes.ok
+            or self.response.status_code == requests.codes.not_modified
+        )
+
+    def data_not_modified(self):
+        return self.response.status_code == requests.codes.not_modified
+
+    def updateCache(self, **kwargs):
+        self.__updateHeaders(**kwargs)
+
+    def __updateHeaders(self, **headers):
+        self.headers.update(**headers)
+
 
 @ratelimit(key="ip", rate="100/h")
 def get_anime(request):
-    payload = {
+    params = {
         "q": "",
         "order_by": "members",
         "sort": "desc",
@@ -16,63 +49,62 @@ def get_anime(request):
         "page": "1",
     }
     url = "https://api.jikan.moe/v3/search/anime"
-    if cache.get("index_e_tag"):
-        headers = {"If-None-Match": cache.get("index_e_tag")}
-        response = requests.get(url, headers=headers, params=payload)
+    anime_api = APIService(url, params, "Could not find any anime")
+
+    if cache.get("index"):
+        headers = {"If-None-Match": cache.get("index").get("e_tag")}
+        anime_api.updateCache(**headers)
+        anime_api.get()
     else:
-        response = requests.get(url, params=payload)
-        cache.set("index_e_tag", response.headers.get("ETag"))
-    return JsonResponse(response.json())
+        anime_api.get()
+        cache.set(
+            "index",
+            {
+                "data": anime_api.response.json(),
+                "e_tag": anime_api.response.headers.get("ETag"),
+            },
+        )
+
+    if anime_api.valid_response():
+        user_response = (
+            cache.get("index").get("data")
+            if anime_api.data_not_modified()
+            else anime_api.response.json()
+        )
+    else:
+        user_response = anime_api.error_message
+
+    return JsonResponse(user_response)
 
 
 @ratelimit(key="ip", rate="100/h")
 def find_anime(request, character_name):
-    quote_api_payload = {"name": character_name}
-    quote_api_response = requests.get(
+    quote_api = APIService(
         "https://animechan.vercel.app/api/quotes/character",
-        params=quote_api_payload,
+        {"name": character_name},
+        f"Could not find any quotes for {character_name}",
     )
-    if (
-        quote_api_response.status_code == requests.codes.ok
-        or quote_api_response.status_code
-        == requests.codes.get("If-None-Match")
-    ):
-        # TODO Make sure json contains correct content and not error message
-        quote_api_json = quote_api_response.json()
-        anime_name = quote_api_json.pop(0)["anime"]
-        anime_api_payload = {"q": anime_name}
-        anime_api_url = "https://api.jikan.moe/v3/search/anime"
+    quote_api.get()
+    if quote_api.valid_response():
+        anime_name = quote_api.response.json().pop(0)["anime"]
+        anime_api = APIService(
+            "https://api.jikan.moe/v3/search/anime",
+            {"q": anime_name},
+            f"Could not find any anime for {anime_name}",
+        )
         if cache.get("find_anime_e_tag"):
             headers = {"If-None-Match": cache.get("find_anime_e_tag")}
-            anime_api_response = requests.get(
-                anime_api_url, headers=headers, params=anime_api_payload
-            )
+            anime_api.updateCache(**headers)
+            anime_api.get()
         else:
-            anime_api_response = requests.get(
-                anime_api_url, params=anime_api_payload
-            )
+            anime_api.get()
             cache.set(
-                "find_anime_e_tag", anime_api_response.headers.get("ETag")
+                "find_anime_e_tag", anime_api.response.headers.get("ETag")
             )
 
-        if (
-            anime_api_response.status_code == requests.codes.ok
-            or anime_api_response.status_code
-            == requests.codes.get("If-None-Match")
-        ):
-            # TODO Make sure json contains correct content and not error message
-            return JsonResponse(anime_api_response.json())
+        if anime_api.valid_response():
+            return JsonResponse(anime_api.response.json())
         else:
-            return JsonResponse(
-                {
-                    "quote": f"Could not find any anime for {anime_name}",
-                    "status": "Error",
-                }
-            )
+            return JsonResponse(anime_api.error_message)
     else:
-        return JsonResponse(
-            {
-                "quote": f"Could not find any quotes for {character_name}",
-                "status": "Error",
-            }
-        )
+        return JsonResponse(quote_api.error_message)
